42e93ffae4c2e1aceb918cf57f5fed58da26b901
agahafiz04 <agahafiz04@gmail.com>
2023-09-13 11:14:06 +0700

    add some script to code

--- script/script.js
+++ script/script.js
@@ -1,3 +1,19 @@
 const videoEl = document.querySelector("video");
 
 videoEl.playbackRate = 0.5;
+
+const passIconEl = document.querySelectorAll("#password-icon");
+const userPassEl = document.querySelector("#user-pass");
+const confirmPassEl = document.querySelector("#confirm-pass");
+
+for (const passIcon of passIconEl) {
+  passIcon.addEventListener("click", () => {
+    if (passIcon.className === "fa-solid fa-eye") {
+      passIcon.setAttribute("class", "fa-solid fa-eye-slash");
+    } else {
+      passIcon.setAttribute("class", "fa-solid fa-eye");
+    }
+    //
+    console.log(passIcon.className);
+  });
+}
